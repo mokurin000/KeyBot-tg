e540967fe54413767480cb88b03b5a9a5b73d4bd
poly000 <1348292515a@gmail.com>
2024-10-14 16:15:17 +0800

    enhance: set parse mode for TOS

--- src/keybot_tg/__main__.py
+++ src/keybot_tg/__main__.py
@@ -8,6 +8,7 @@ from telegram import (
     InlineKeyboardMarkup,
     BotCommand,
 )
+from telegram.constants import ParseMode
 from telegram.ext import (
     Application,
     ApplicationBuilder,
@@ -291,7 +292,7 @@ If you have any question, please contact @iwtfll1 and ask for support.
 
 
 async def term_of_service(update: Update, _context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(TOS)
+    await update.message.reply_text(TOS, parse_mode=ParseMode.MARKDOWN_V2)
 
 
 def main():
